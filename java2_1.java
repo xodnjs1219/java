05dd946df7fbcee77f74699c13c4717639b21059
xodnjs1219 <102015461+xodnjs1219@users.noreply.github.com>
2022-05-05 15:21:41 +0900

    Create java2_1.java

---
+++ java2_1.java
@@ -0,0 +1,13 @@
+public class java_21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//print-줄바꿈x
+		//println-줄바꿈
+		System.out.println(3-5); 
+		System.out.println(3*5);
+		System.out.println(3+5);
+		System.out.println(3/5);
+	}
+
+}
